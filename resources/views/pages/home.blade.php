87b2524937834616199b4a847eabac5d44a3ad95
RominaMalimban <romina.lyn@hotmail.it>
2023-01-31 18:33:03 +0100

    bonus 1 e 2

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,11 +1,35 @@
 @extends('layouts.main-layout')
 
 @section('content')
-    <h1>Movies List</h1>
 
-    <ul>
-        @foreach ($movies as $movie)
-            <li>{{$movie['title']}}</li>
-        @endforeach
-    </ul>
+    <div class="container">
+        <h1 class="my-4">Movies List</h1>
+
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Title</th>
+                    <th>Original title</th>
+                    <th>Nationality</th>
+                    <th>Date</th>
+                    <th>Vote</th>
+                </tr>
+            </thead>
+            
+            <tbody class="table-group-divider">
+                @foreach ($movies as $movie)
+                    <tr>
+                        <td>{{ $movie['id']}}</td>
+                        <td>{{ $movie['title']}}</td>
+                        <td>{{ $movie['original_title']}}</td>
+                        <td>{{ $movie['nationality']}}</td>
+                        <td>{{ $movie['date']}}</td>
+                        <td>{{ $movie['vote']}}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+    
 @endsection
